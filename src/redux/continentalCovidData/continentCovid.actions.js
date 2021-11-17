8ff80fb28683bd4f4c5be8a274b3a224f974254b
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-11-17 20:18:02 +0200

    Add tests

--- src/redux/continentalCovidData/continentCovid.actions.js
+++ src/redux/continentalCovidData/continentCovid.actions.js
@@ -7,30 +7,29 @@ import {
 } from './continentCovid.types';
 
 export const requestCovidData = () => ({
- type: REQUEST_COVID_DATA,
+  type: REQUEST_COVID_DATA,
 });
 
-
 export const requestCovidDataSucess = (data) => ({
- type: REQUEST_COVID_DATA_SUCCESS,
- payload: data,
+  type: REQUEST_COVID_DATA_SUCCESS,
+  payload: data,
 });
 
 export const errorRequestingCovidData = (error) => ({
- type: ERROR_REQUESTING_COVID_DATA,
- payload: error,
+  type: ERROR_REQUESTING_COVID_DATA,
+  payload: error,
 });
 
 const fetchCovidData = (country) => async (dispatch) => {
- dispatch(requestCovidData());
- axios.get(getContinent(country))
-   .then((response) => {
-     const result = Object.values(response.data);
+  dispatch(requestCovidData());
+  axios.get(getContinent(country))
+    .then((response) => {
+      const result = Object.values(response.data);
 
-     dispatch(requestCovidDataSucess(result));
-   }).catch((error) => {
-     dispatch(errorRequestingCovidData(error));
-   });
+      dispatch(requestCovidDataSucess(result));
+    }).catch((error) => {
+      dispatch(errorRequestingCovidData(error));
+    });
 };
 
 export default fetchCovidData;
--- src/redux/continentalCovidData/continentCovid.reducer.js
+++ src/redux/continentalCovidData/continentCovid.reducer.js
@@ -1,24 +1,24 @@
 import {
- REQUEST_COVID_DATA,
- REQUEST_COVID_DATA_SUCCESS,
- ERROR_REQUESTING_COVID_DATA,
+  REQUEST_COVID_DATA,
+  REQUEST_COVID_DATA_SUCCESS,
+  ERROR_REQUESTING_COVID_DATA,
 } from './continentCovid.types';
 
 const initialState = {
- isLoading: false,
- covidData: [],
- error: '',
+  isLoading: false,
+  covidData: [],
+  error: '',
 };
 
 const covidReducer = (state = initialState, action) => {
- switch (action.type) {
-   case REQUEST_COVID_DATA:
-     return {
-       ...state,
-       isLoading: true,
-     };
+  switch (action.type) {
+    case REQUEST_COVID_DATA:
+      return {
+        ...state,
+        isLoading: true,
+      };
 
-     case REQUEST_COVID_DATA_SUCCESS:
+    case REQUEST_COVID_DATA_SUCCESS:
       return {
         ...state,
         isLoading: false,
