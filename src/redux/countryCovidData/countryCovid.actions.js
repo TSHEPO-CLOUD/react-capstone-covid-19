ad8fe1b79e28d5d30ad0d3963e205610c042e30d
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-11-17 11:18:17 +0200

    Render App with paths

--- src/redux/countryCovidData/countryCovid.actions.js
+++ src/redux/countryCovidData/countryCovid.actions.js
@@ -7,13 +7,29 @@ import {
 } from './countryCovid.types';
 
 export const requestCountryCovidData = () => ({
- 
+  type: REQUEST_COUNTRY_COVID_DATA,
 });
 
 export const requestCountryCovidDataSuccess = (payload) => ({
- 
+  type: REQUEST_COUNTRY_COVID_DATA_SUCCESS,
+  payload,
 });
 
 export const requestCountryCovidDataFailure = (error) => ({
- 
+  type: REQUEST_COUNTRY_COVID_DATA_FAILURE,
+  payload: error,
 });
+
+const fetchCountryData = (country) => (dispatch) => {
+  dispatch(requestCountryCovidData());
+
+  axios.get(getCountry(country))
+    .then((response) => {
+      const result = Object.values(response.data);
+      dispatch(requestCountryCovidDataSuccess(result));
+    }).catch((error) => {
+      dispatch(requestCountryCovidDataFailure(error));
+    });
+};
+
+export default fetchCountryData;
