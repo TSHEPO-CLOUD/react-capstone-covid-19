b447c4b7096c8641be9f75483eedbe23846dd688
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-11-17 11:03:50 +0200

    Add request data

--- src/redux/continentalCovidData/continentCovid.actions.js
+++ src/redux/continentalCovidData/continentCovid.actions.js
@@ -5,3 +5,12 @@ import {
   REQUEST_COVID_DATA_SUCCESS,
   ERROR_REQUESTING_COVID_DATA,
 } from './continentCovid.types';
+
+export const requestCovidData = () => ({
+ type: REQUEST_COVID_DATA,
+});
+
+export const errorRequestingCovidData = (error) => ({
+ type: ERROR_REQUESTING_COVID_DATA,
+ payload: error,
+});
