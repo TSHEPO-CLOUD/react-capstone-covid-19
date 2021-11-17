da26597487f8870c7d62746ecc6e75c86c7ed82f
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-11-17 11:08:19 +0200

    Covid reducer

--- src/redux/continentalCovidData/continentCovid.reducer.js
+++ src/redux/continentalCovidData/continentCovid.reducer.js
@@ -3,3 +3,24 @@ import {
  REQUEST_COVID_DATA_SUCCESS,
  ERROR_REQUESTING_COVID_DATA,
 } from './continentCovid.types';
+
+const initialState = {
+ isLoading: false,
+ covidData: [],
+ error: '',
+};
+
+const covidReducer = (state = initialState, action) => {
+ switch (action.type) {
+   case REQUEST_COVID_DATA:
+     return {
+       ...state,
+       isLoading: true,
+     };
+
+     case REQUEST_COVID_DATA_SUCCESS:
+
+
+
+
+      case ERROR_REQUESTING_COVID_DATA:
